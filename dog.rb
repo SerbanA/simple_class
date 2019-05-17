a19aa507dd1801652d35b9af223f9810b8b76e29
SerbanA <adyserban97@yahoo.com>
2019-05-17 10:39:01 +0300

    Separated class into different file

--- dog.rb
+++ dog.rb
@@ -22,14 +22,4 @@ class Dog
   end
 end
 
-dog1 = Dog.new("Cho","Akita","big","good")
-dog2 = Dog.new("Buck","Bulldog","medium","good")
-dog3 = Dog.new("Huan","chihuahua","small","Evil Incarnate")
-
-dog1.bark
-dog1.intro
-dog2.bark
-dog2.intro
-dog3.bark
-dog3.intro
 
---
+++ main.rb
@@ -0,0 +1,13 @@
+require_relative "dog.rb"
+
+dog1 = Dog.new("Cho","Akita","big","good")
+dog2 = Dog.new("Buck","Bulldog","medium","good")
+dog3 = Dog.new("Huan","chihuahua","small","Evil Incarnate")
+
+
+dog1.bark
+dog1.intro
+dog2.bark
+dog2.intro
+dog3.bark
+dog3.intro
